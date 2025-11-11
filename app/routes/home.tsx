3c5e262a741e764ece4a829d753091e3e51de4a0
mac <mac@MacBook-Pro-di-mac.local>
2025-11-11 19:15:29 +0100

    Navbar Initiated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
+import Navbar from '../components/Navbar'; // Don't forget this import!
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: "Resumer" },
+        { name: "description", content: "Your powerful tool to get that Job" },
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('images/bg-main.svg')] bg-cover">
+
+        <Navbar />
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>Track Your Applications and Resume</h1>
+                <h2>Review Your Submissions and check AI-Powered feedback.</h2>
+            </div>
+        </section>
+    </main>
 }
